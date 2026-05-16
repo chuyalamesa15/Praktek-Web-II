4f25a1dd083f52e5a12615fe83eaec1a49053261
chuyaa <chuyalamesa@gmail.com>
2026-05-16 21:49:54 +0800

    Add files via upload

---
+++ components/logout.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="logoutModal" tabindex="-1">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+
+                <h5 class="modal-title">
+                    Logout
+                </h5>
+
+            </div>
+
+            <div class="modal-body">
+
+                Yakin ingin logout?
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
